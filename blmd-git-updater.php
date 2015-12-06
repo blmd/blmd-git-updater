70f54094c02fcdbda959072b94a5184e1d94d564
Casey <caseyjvcain@gmail.com>
2015-12-06 16:48:44 -0500

    Initial import

---
+++ blmd-git-updater.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: BLMD Git Updater
+Plugin URI: https://github.com/blmd/blmd-git-updater
+Description: Allow git updates
+Author: blmd
+Author URI: https://github.com/blmd
+Version: 0.1
+*/
+
+!defined( 'ABSPATH' ) && die;
+define( 'BLMD_GIT_UPDATER_VERSION', '0.1' );
+define( 'BLMD_GIT_UPDATER_URL', plugin_dir_url( __FILE__ ) );
+define( 'BLMD_GIT_UPDATER_DIR', plugin_dir_path( __FILE__ ) );
+define( 'BLMD_GIT_UPDATER_BASENAME', plugin_basename( __FILE__ ) );
+
